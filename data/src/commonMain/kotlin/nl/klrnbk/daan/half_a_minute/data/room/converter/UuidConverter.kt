760661b747559e5fa28d2b5751fba729ae900c43
Daan <daan@klrnbk.nl>
2025-11-26 10:09:40 +0100

    feat(Data): add Uuid converter

---
+++ data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/room/converter/UuidConverter.kt
@@ -0,0 +1,14 @@
+package nl.klrnbk.daan.half_a_minute.data.room.converter
+
+import androidx.room.TypeConverter
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
+
+@OptIn(ExperimentalUuidApi::class)
+class UuidConverter {
+    @TypeConverter
+    fun fromUUID(uuid: Uuid): String = uuid.toString()
+
+    @TypeConverter
+    fun uuidFromString(string: String): Uuid = Uuid.parse(string)
+}
