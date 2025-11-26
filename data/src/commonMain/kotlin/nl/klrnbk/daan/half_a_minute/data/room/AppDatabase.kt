74194b1702d06b669a958ed0012af5a167f9ac42
Daan <daan@klrnbk.nl>
2025-11-26 10:02:48 +0100

    feat(Data): add Uuid converter

--- data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/room/AppDatabase.kt
+++ data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/room/AppDatabase.kt
@@ -6,13 +6,14 @@ import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
 import androidx.room.TypeConverters
 import nl.klrnbk.daan.half_a_minute.data.room.converter.GameStatusConverter
+import nl.klrnbk.daan.half_a_minute.data.room.converter.UuidConverter
 
 @Database(
     entities = [],
     version = 1
 )
 @ConstructedBy(AppDatabaseConstructor::class)
-@TypeConverters(GameStatusConverter::class)
+@TypeConverters(GameStatusConverter::class, UuidConverter::class)
 abstract class AppDatabase : RoomDatabase()
 
 @Suppress("NO_ACTUAL_FOR_EXPECT")
