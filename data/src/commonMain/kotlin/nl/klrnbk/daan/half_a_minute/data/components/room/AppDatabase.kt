d9baab5f38584d4e2eb62cb54aafa8d14b048d23
Daan <daan@klrnbk.nl>
2025-11-26 10:23:13 +0100

    feat(AppDatabase): add missing links

--- data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/components/room/AppDatabase.kt
+++ data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/components/room/AppDatabase.kt
@@ -5,11 +5,16 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.RoomDatabaseConstructor
 import androidx.room.TypeConverters
-import nl.klrnbk.daan.half_a_minute.data.room.converter.GameStatusConverter
-import nl.klrnbk.daan.half_a_minute.data.room.converter.UuidConverter
+import nl.klrnbk.daan.half_a_minute.data.components.room.converter.GameStatusConverter
+import nl.klrnbk.daan.half_a_minute.data.components.room.converter.UuidConverter
+import nl.klrnbk.daan.half_a_minute.data.components.room.dao.GameDao
+import nl.klrnbk.daan.half_a_minute.data.components.room.dao.PlayerDao
+import nl.klrnbk.daan.half_a_minute.data.components.room.entity.GameEntity
+import nl.klrnbk.daan.half_a_minute.data.components.room.entity.PlayerEntity
+import nl.klrnbk.daan.half_a_minute.data.components.room.entity.TeamEntity
 
 @Database(
-    entities = [],
+    entities = [GameEntity::class, TeamEntity::class, PlayerEntity::class],
     version = 1
 )
 @ConstructedBy(AppDatabaseConstructor::class)
@@ -19,4 +24,6 @@ abstract class AppDatabase : RoomDatabase()
 @Suppress("NO_ACTUAL_FOR_EXPECT")
 expect object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
     override fun initialize(): AppDatabase
+    fun gameDao(): GameDao
+    fun playerDao(): PlayerDao
 }
