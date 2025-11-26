b99955221114667de27e6c00d0e284e46e5f7694
Daan <daan@klrnbk.nl>
2025-11-26 08:49:36 +0100

    feat(Data): setup database config

--- data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/config/DataConfig.kt
+++ data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/config/DataConfig.kt
@@ -1,6 +1,7 @@
 package nl.klrnbk.daan.half_a_minute.data.config
 
 object DataConfig {
+    const val LOCAL_DB_NAME = "half_a_minute.db"
 
     const val EXAMPLE_API_BASE_URL = "https://enteryourapibaseurlhere.com"
 }
---
+++ data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/room/AppDatabaseConfig.kt
@@ -0,0 +1,3 @@
+package nl.klrnbk.daan.half_a_minute.data.room
+
+data class AppDatabaseConfig(val localDbName: String)
