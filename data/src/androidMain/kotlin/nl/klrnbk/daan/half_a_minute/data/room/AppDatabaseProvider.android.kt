c693f3ed0f4262956ac8636f539dfb59571a706a
Daan <daan@klrnbk.nl>
2025-11-26 08:49:57 +0100

    feat(Data): setup database providers

---
+++ data/src/androidMain/kotlin/nl/klrnbk/daan/half_a_minute/data/room/AppDatabaseProvider.android.kt
@@ -0,0 +1,22 @@
+package nl.klrnbk.daan.half_a_minute.data.room
+
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import nl.klrnbk.daan.half_a_minute.data.context.ContextWrapper
+import nl.klrnbk.daan.half_a_minute.data.di.qualifiers.LocalAppDatabaseConfig
+import org.koin.core.annotation.Single
+
+@Single
+actual class AppDatabaseProvider(
+    private val contextWrapper: ContextWrapper,
+    @LocalAppDatabaseConfig private val appDatabaseConfig: AppDatabaseConfig
+) {
+    actual fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
+        val appContext = contextWrapper.get().applicationContext
+        val dbFile = appContext.getDatabasePath(appDatabaseConfig.localDbName)
+        return Room.databaseBuilder<AppDatabase>(
+            context = appContext,
+            name = dbFile.absolutePath
+        )
+    }
+}
---
+++ data/src/iosMain/kotlin/nl/klrnbk/daan/half_a_minute/data/room/AppDatabaseProvider.ios.kt
@@ -0,0 +1,34 @@
+package nl.klrnbk.daan.half_a_minute.data.room
+
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlinx.cinterop.ExperimentalForeignApi
+import nl.klrnbk.daan.half_a_minute.data.di.qualifiers.LocalAppDatabaseConfig
+import org.koin.core.annotation.Single
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
+
+@Single
+actual class AppDatabaseProvider(
+    @LocalAppDatabaseConfig private val appDatabaseConfig: AppDatabaseConfig
+) {
+    actual fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
+        val dbFilePath = documentDirectory() + appDatabaseConfig.localDbName
+        return Room.databaseBuilder<AppDatabase>(
+            name = dbFilePath
+        )
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    private fun documentDirectory(): String {
+        val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+            directory = NSDocumentDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = false,
+            error = null
+        )
+        return requireNotNull(documentDirectory?.path)
+    }
+}
