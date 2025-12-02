e9a280b6c7306cf13a4f186abc18f72333a44bb7
Daan <daan@klrnbk.nl>
2025-12-02 13:29:45 +0100

    feat: add CRUD options for players entity

--- data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/components/room/dao/PlayerDao.kt
+++ data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/components/room/dao/PlayerDao.kt
@@ -3,17 +3,25 @@ package nl.klrnbk.daan.half_a_minute.data.components.room.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.Update
+import androidx.room.Query
+import androidx.room.Transaction
+import kotlin.uuid.Uuid
 import nl.klrnbk.daan.half_a_minute.data.components.room.entity.PlayerEntity
 
 @Dao
 interface PlayerDao {
+    @Transaction
+    @Query("SELECT * FROM game WHERE game.id = :id")
+    suspend fun getById(id: Uuid): PlayerEntity?
+
     @Insert
     suspend fun insert(entity: PlayerEntity)
 
-    @Update
-    suspend fun update(entity: PlayerEntity)
+    @Transaction
+    @Query("UPDATE player SET teamId = :teamId WHERE player.id = :playerId")
+    suspend fun addPlayerToTeam(playerId: Uuid, teamId: Uuid): PlayerEntity?
 
-    @Delete
-    suspend fun delete(entity: PlayerEntity)
+    @Transaction
+    @Query("DELETE FROM player WHERE player.id = :id")
+    suspend fun deleteById(id: Uuid)
 }
