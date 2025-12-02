415138a3df8775a322b5c84184cfa3aa9c6cc99b
Daan <daan@klrnbk.nl>
2025-12-02 11:22:15 +0100

    feat(Data): add host to 'GameWithTTeamsPlayersAndHost' relation

--- data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/components/room/dao/relations/GameWithTeamsPlayersAndHost.kt
+++ data/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/data/components/room/dao/relations/GameWithTeamsPlayersAndHost.kt
@@ -15,6 +15,12 @@ data class GameWithTeamsPlayersAndHost(
     val players: List<PlayerEntity>,
 
     @Relation(
+        parentColumn = "hostId",
+        entityColumn = "id"
+    )
+    val host: PlayerEntity?,
+
+    @Relation(
         entity = TeamEntity::class,
         parentColumn = "id",
         entityColumn = "gameId"
