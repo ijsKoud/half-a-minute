e3b5c2b953a47c1a0cd703b3f2fd0b1aa7f62e33
Daan <daan@klrnbk.nl>
2025-12-02 11:21:54 +0100

    chore: remove @OptIns

--- domain/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/domain/game/model/Game.kt
+++ domain/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/domain/game/model/Game.kt
@@ -1,6 +1,5 @@
 package nl.klrnbk.daan.half_a_minute.domain.game.model
 
-import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 
 enum class GameStatus {
@@ -8,11 +7,10 @@ enum class GameStatus {
     COMPLETED
 }
 
-@OptIn(ExperimentalUuidApi::class)
 data class Game(
     val id: Uuid,
     val status: GameStatus,
-    val host: Player,
+    val host: Player?,
     val teams: List<Team>,
     val players: List<Player>
 )
