ab884bf9282bc390b75fd99d7b37ea652cd19675
Daan <daan@klrnbk.nl>
2025-12-07 00:44:40 +0100

    refactor(TextInput): move error message to designated 'supportMessage' property on Input

--- presentation/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/presentation/ui/components/input/TextInput.kt
+++ presentation/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/presentation/ui/components/input/TextInput.kt
@@ -3,6 +3,7 @@ package nl.klrnbk.daan.half_a_minute.presentation.ui.components.input
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -47,86 +48,82 @@ fun TextInput(
     interactionSource: MutableInteractionSource? = null,
     style: InteractionStyle = AppTheme.colors.input.default
 ) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(0.dp)
-    ) {
-        TextField(
-            modifier = modifier,
-            value = value,
-            onValueChange = onValueChange,
-            enabled = enabled,
-            readOnly = readOnly,
-            label = label,
-            placeholder = placeholder,
-            leadingIcon = leadingIcon,
-            trailingIcon = trailingIcon,
-            prefix = prefix,
-            suffix = suffix,
-            supportingText = {},
-            isError = errorMessage != null,
-            visualTransformation = visualTransformation,
-            keyboardOptions = keyboardOptions,
-            keyboardActions = keyboardActions,
-            singleLine = singleLine,
-            maxLines = maxLines,
-            minLines = minLines,
-            interactionSource = interactionSource,
-            shape = RoundedCornerShape(Dimension.Input.borderRadius),
-            colors = TextFieldDefaults.colors(
-                disabledTextColor = style.text.muted,
-                disabledContainerColor = style.background.muted,
-                disabledPrefixColor = style.text.muted,
-                disabledLabelColor = style.text.muted,
-                disabledSuffixColor = style.text.muted,
-                disabledPlaceholderColor = style.text.muted,
-                disabledLeadingIconColor = style.text.muted,
-                disabledTrailingIconColor = style.text.muted,
-                errorTextColor = AppTheme.colors.input.danger.text.base,
-                errorSupportingTextColor = AppTheme.colors.input.danger.background.base,
-                errorContainerColor = AppTheme.colors.input.danger.background.base,
-                errorLabelColor = AppTheme.colors.input.danger.background.base,
-                errorCursorColor = AppTheme.colors.input.danger.text.base,
-                errorPrefixColor = AppTheme.colors.input.danger.text.base,
-                errorSuffixColor = AppTheme.colors.input.danger.text.base,
-                errorIndicatorColor = Color.Transparent,
-                errorPlaceholderColor = AppTheme.colors.input.danger.text.base,
-                errorLeadingIconColor = AppTheme.colors.input.danger.text.base,
-                errorTrailingIconColor = AppTheme.colors.input.danger.text.base,
-                focusedContainerColor = style.background.base,
-                unfocusedContainerColor = style.background.base,
-                focusedTextColor = style.text.base,
-                unfocusedTextColor = style.text.base,
-                focusedLabelColor = style.text.base,
-                unfocusedLabelColor = style.text.base,
-                focusedPrefixColor = style.text.base,
-                unfocusedPrefixColor = style.text.base,
-                focusedSuffixColor = style.text.base,
-                unfocusedSuffixColor = style.text.base,
-                focusedTrailingIconColor = style.text.base,
-                unfocusedTrailingIconColor = style.text.base,
-                focusedLeadingIconColor = style.text.base,
-                unfocusedLeadingIconColor = style.text.base,
-                focusedPlaceholderColor = style.text.muted,
-                unfocusedPlaceholderColor = style.text.muted,
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent
-            )
-        )
+    TextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        enabled = enabled,
+        readOnly = readOnly,
+        label = label,
+        placeholder = placeholder,
+        leadingIcon = leadingIcon,
+        trailingIcon = trailingIcon,
+        prefix = prefix,
+        suffix = suffix,
+        supportingText = {
+            if (errorMessage != null) {
+                val color = if (enabled) {
+                    AppTheme.colors.input.danger.background.base
+                } else {
+                    AppTheme.colors.input.danger.background.muted
+                }
 
-        if (errorMessage != null) {
-            val color = if (enabled) {
-                AppTheme.colors.input.danger.background.base
-            } else {
-                AppTheme.colors.input.danger.background.muted
+                Text(
+                    text = errorMessage,
+                    color = color,
+                    style = AppTheme.typography.inputSupportMessage,
+                    modifier = Modifier.padding(vertical = Dimension.Padding.small)
+                )
             }
-
-            Text(
-                text = errorMessage,
-                color = color,
-                style = AppTheme.typography.inputSupportMessage,
-                modifier = Modifier.padding(bottom = Dimension.Padding.small)
-            )
-        }
-    }
+        },
+        isError = errorMessage != null,
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        singleLine = singleLine,
+        maxLines = maxLines,
+        minLines = minLines,
+        interactionSource = interactionSource,
+        shape = RoundedCornerShape(Dimension.Input.borderRadius),
+        colors = TextFieldDefaults.colors(
+            disabledTextColor = style.text.muted,
+            disabledContainerColor = style.background.muted,
+            disabledPrefixColor = style.text.muted,
+            disabledLabelColor = style.text.muted,
+            disabledSuffixColor = style.text.muted,
+            disabledPlaceholderColor = style.text.muted,
+            disabledLeadingIconColor = style.text.muted,
+            disabledTrailingIconColor = style.text.muted,
+            errorTextColor = AppTheme.colors.input.danger.text.base,
+            errorSupportingTextColor = AppTheme.colors.input.danger.background.base,
+            errorContainerColor = AppTheme.colors.input.danger.background.base,
+            errorLabelColor = AppTheme.colors.input.danger.background.base,
+            errorCursorColor = AppTheme.colors.input.danger.text.base,
+            errorPrefixColor = AppTheme.colors.input.danger.text.base,
+            errorSuffixColor = AppTheme.colors.input.danger.text.base,
+            errorIndicatorColor = Color.Transparent,
+            errorPlaceholderColor = AppTheme.colors.input.danger.text.base,
+            errorLeadingIconColor = AppTheme.colors.input.danger.text.base,
+            errorTrailingIconColor = AppTheme.colors.input.danger.text.base,
+            focusedContainerColor = style.background.base,
+            unfocusedContainerColor = style.background.base,
+            focusedTextColor = style.text.base,
+            unfocusedTextColor = style.text.base,
+            focusedLabelColor = style.text.base,
+            unfocusedLabelColor = style.text.base,
+            focusedPrefixColor = style.text.base,
+            unfocusedPrefixColor = style.text.base,
+            focusedSuffixColor = style.text.base,
+            unfocusedSuffixColor = style.text.base,
+            focusedTrailingIconColor = style.text.base,
+            unfocusedTrailingIconColor = style.text.base,
+            focusedLeadingIconColor = style.text.base,
+            unfocusedLeadingIconColor = style.text.base,
+            focusedPlaceholderColor = style.text.muted,
+            unfocusedPlaceholderColor = style.text.muted,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent
+        )
+    )
 }
