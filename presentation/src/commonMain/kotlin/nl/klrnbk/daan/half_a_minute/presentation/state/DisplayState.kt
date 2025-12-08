b73ec3f7f787a1d5ca94bf2c7f29397e8543eaf2
Daan <daan@klrnbk.nl>
2025-12-09 00:20:40 +0100

    feat(DisplayState): update naming

--- presentation/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/presentation/state/DisplayState.kt
+++ presentation/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/presentation/state/DisplayState.kt
@@ -1,6 +1,6 @@
 package nl.klrnbk.daan.half_a_minute.presentation.state
 
 sealed interface DisplayState<out T>
-class OnResult<out T>(val result: T) : DisplayState<T>
-class OnError(val errorState: ErrorDisplayState) : DisplayState<Nothing>
-data object OnLoading : DisplayState<Nothing>
+class ResultState<out T>(val result: T) : DisplayState<T>
+class ErrorState(val error: ErrorDisplayState) : DisplayState<Nothing>
+class LoadingState : DisplayState<Nothing>
