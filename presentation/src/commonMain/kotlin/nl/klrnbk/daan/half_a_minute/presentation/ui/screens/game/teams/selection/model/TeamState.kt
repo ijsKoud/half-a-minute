f7c84cf1a15c9f605263c0c346c5db01dc2cfaac
Daan <daan@klrnbk.nl>
2025-12-09 13:35:38 +0100

    feat(TeamSelectionScreen): filter availability based on selections

---
+++ presentation/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/presentation/ui/screens/game/teams/selection/model/TeamState.kt
@@ -0,0 +1,5 @@
+package nl.klrnbk.daan.half_a_minute.presentation.ui.screens.game.teams.selection.model
+
+import nl.klrnbk.daan.half_a_minute.domain.game.model.TeamName
+
+data class TeamState(val name: TeamName, val disabled: Boolean)
