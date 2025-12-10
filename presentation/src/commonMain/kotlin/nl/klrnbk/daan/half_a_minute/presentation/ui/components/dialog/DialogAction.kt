1295f4abfc25ec6d3938a12a62f29d4f4261d62c
Daan <daan@klrnbk.nl>
2025-12-10 13:31:07 +0100

    feat: add Android implementation of dialog

---
+++ presentation/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/presentation/ui/components/dialog/DialogAction.kt
@@ -0,0 +1,13 @@
+package nl.klrnbk.daan.half_a_minute.presentation.ui.components.dialog
+
+data class DialogAction(
+    val name: String,
+    val isPrimary: Boolean,
+    val type: DialogActionType,
+    val action: () -> Unit
+)
+
+enum class DialogActionType {
+    SUBMIT,
+    DISMISS
+}
