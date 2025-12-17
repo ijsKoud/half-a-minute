5e8ca341f0f4e5f2ea9fa55d3f0f970c73cf4a4c
Daan <daan@klrnbk.nl>
2025-12-17 09:31:36 +0100

    fix: route causes crashes becasue of invalid type

--- presentation/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/presentation/navigation/GameIndividualTeamSelectionNavigation.kt
+++ presentation/src/commonMain/kotlin/nl/klrnbk/daan/half_a_minute/presentation/navigation/GameIndividualTeamSelectionNavigation.kt
@@ -10,7 +10,7 @@ import nl.klrnbk.daan.half_a_minute.presentation.ui.screens.game.teams.individua
 private const val GameIdArg = "gameId"
 private const val PlayerIdArg = "playerId"
 
-const val GameIndividualTeamSelectionRoute = "/game/{$GameIdArg}/teams/selection/{$PlayerIdArg}"
+const val GameIndividualTeamSelectionRoute = "/game/{$GameIdArg}/teams/selection/player/{$PlayerIdArg}"
 
 internal class GameIndividualTeamSelectionArgs(val gameId: Uuid, val playerId: Uuid) {
     constructor(savedStateHandle: SavedStateHandle) :
